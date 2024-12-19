b704353943fbe77d6c0a53ab80f26b70e915fa2e
mirzayogy <mirza.yogy@gmail.com>
2024-12-19 08:45:36 +0800

    fix routing

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome', ["title" => "Dashboard"])
-    ->name('dashboard');
-});
+    return view('welcome', ["title" => "Dashboard"]);
+})->name('dashboard');
 
 Route::get('/sample', function () {
-    return view('sample', ["title" => "Sample"])
-    ->name('sample');
-});
+    return view('sample', ["title" => "Sample"]);
+})->name('sample');
