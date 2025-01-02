f9e8949cdc0aa503c03622022b122c152117d11c
mirzayogy <mirza.yogy@gmail.com>
2025-01-02 12:17:52 +0800

    php artisan make:factory BarangMasukFactory

--- database/migrations/2025_01_01_140954_create_barang_masuks_table.php
+++ database/migrations/2025_01_01_140954_create_barang_masuks_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('barang_masuks', function (Blueprint $table) {
             $table->id();
+            $table->date('tanggal');
+            $table->bigInteger('pemasok_id');
+            $table->bigInteger('pengguna_id');
             $table->timestamps();
         });
     }
